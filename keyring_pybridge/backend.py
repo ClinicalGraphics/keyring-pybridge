5fd8894c9d867e2d76cb78f9e303e3ceec7306ae
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2023-07-23 07:21:33 +0100

    Check if python executable is not self
    Prevent infinite loops

--- keyring_pybridge/backend.py
+++ keyring_pybridge/backend.py
@@ -1,5 +1,9 @@
 import json
+from functools import cache
+from pathlib import Path
+from shutil import which
 from subprocess import run
+from sys import executable
 
 from keyring.backend import KeyringBackend
 
@@ -16,6 +20,18 @@ def call_python_keyring(python, command):
     return stdout
 
 
+@cache
+def check_python(python):
+    py_self = Path(executable).resolve()
+    py_bridge = Path(python).resolve()
+    if py_bridge.is_file() and py_self == py_bridge:
+        raise ValueError(f"please configure KEYRING_PROPERTY_PYTHON to a different python executable than {executable}")
+    py_bridge = Path(which(python)).resolve()
+    if py_bridge.is_file() and py_self == py_bridge:
+        raise ValueError(f"please configure KEYRING_PROPERTY_PYTHON to a different python executable than {executable}")
+    raise ValueError(f"{python} does not exist")
+
+
 def format_args(*args):
     return ', '.join(map(repr, args))
 
@@ -25,17 +41,20 @@ class PyBridgeKeyring(KeyringBackend):
     python = "python"
 
     def set_password(self, servicename, username, password):
+        check_python(self.python)
         call_python_keyring(
             self.python,
             f"keyring.set_password({format_args(servicename, username, password)})",
         )
 
     def get_password(self, servicename, username):
+        check_python(self.python)
         return json.loads(call_python_keyring(
             self.python, f"import json; print(json.dumps(keyring.get_password({format_args(servicename, username)})))"
         ))
 
     def delete_password(self, servicename, username):
+        check_python(self.python)
         call_python_keyring(
             self.python, f"keyring.delete_password({format_args(servicename, username)})"
         )
