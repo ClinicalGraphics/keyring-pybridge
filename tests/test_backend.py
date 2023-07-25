54fd300a5efc7a747e4f5b783d436bad3ef9f08f
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2023-07-25 15:07:44 +0200

    Correct requirements; UTF-8 support; error handling (#5)
    * Correct requirements and bump version
    
    * Bump version
    
    * fix bugs and complete tests

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,51 +1,54 @@
+from subprocess import CalledProcessError
 from sys import executable
 
 import pytest
 
+from keyring.errors import PasswordDeleteError
 import keyring_pybridge
 
 
 @pytest.fixture
-def backend(monkeypatch):
+def no_check(monkeypatch):
     # disable check_python
     monkeypatch.setattr(keyring_pybridge.backend, "check_python", lambda python: None)
-    # configure current python executable for testing
-    monkeypatch.setattr(keyring_pybridge.PyBridgeKeyring, "python", executable)
 
-    yield keyring_pybridge.PyBridgeKeyring()
+    yield
 
 
 @pytest.fixture
-def backend_misconfigured(monkeypatch):
-    # configure current python executable for testing
+def backend(monkeypatch):
     monkeypatch.setattr(keyring_pybridge.PyBridgeKeyring, "python", executable)
 
     yield keyring_pybridge.PyBridgeKeyring()
 
 
-servicename = "__keyring_pybridge_test"
-username = "username"
-password = "password"
+servicename = "__keyring_pybridge_test€€$ß"
+username = "userna€€$ßme"
+password = "passw€€$ßrd"
 
 
-def test_backend(backend):
+def test_backend(backend, no_check):
+    try:
+        backend.delete_password(servicename, username)
+    except PasswordDeleteError:
+        pass
     assert backend.get_password(servicename, username) is None
     backend.set_password(servicename, username, password)
     assert backend.get_password(servicename, username) == password
     backend.delete_password(servicename, username)
     assert backend.get_password(servicename, username) is None
 
-    with pytest.raises(RuntimeError):
+    with pytest.raises(CalledProcessError):
         assert backend.get_password(None, None) is None
 
 
-def test_backend_same_python(backend_misconfigured):
+def test_backend_same_python(backend):
     with pytest.raises(ValueError):
-        backend_misconfigured.get_password(servicename, username)
+        backend.get_password(servicename, username)
 
 
 def test_format_args():
-    assert keyring_pybridge.backend.format_args("foo", 1, None) == "'foo', 1, None"
+    assert keyring_pybridge.backend.format_args("fooß", 1, None) == "'fooß', 1, None"
 
 
 garble = "saddfasdfasdf"
@@ -63,11 +66,11 @@ def test_check_python():
         keyring_pybridge.backend.check_python(garble)
 
 
-def test_call_python_keyring():
-    assert keyring_pybridge.backend.call_python_keyring(executable, "print(5)") == "5"
+def test_call_keyring(no_check):
+    assert keyring_pybridge.backend.call_keyring(executable, "print('5ß')") == "5ß"
 
     with pytest.raises(FileNotFoundError):
-        keyring_pybridge.backend.call_python_keyring(garble, "")
+        keyring_pybridge.backend.call_keyring(garble, "")
 
-    with pytest.raises(RuntimeError, match=garble):
-        keyring_pybridge.backend.call_python_keyring(executable, garble)
+    with pytest.raises(CalledProcessError, match=garble):
+        keyring_pybridge.backend.call_keyring(executable, garble)
